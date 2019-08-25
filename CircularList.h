3c536b1fd924f9cee7c9a75ce10dd98e8722e27c
Anupam Srivastava <anupam@dont.tech>
2019-08-25 12:36:48 +0530

    Compiling but crashing

---
+++ CircularList.h
@@ -0,0 +1,215 @@
+#include <bitset>
+#include <stdexcept>
+
+template<typename DataType>
+struct ListNode
+{
+    DataType  value;
+    ListNode *next;
+};
+template<typename DataType>
+class ListAllocator
+{
+    typedef ListNode<DataType> Node;
+
+public:
+    template<class T>
+    using rebind = ListAllocator<T>;
+    DataType* allocate(size_t size)
+    {
+        DataType *p = nullptr;
+        size_t    i = 0;
+
+        for (; i < indexMap.size(); i++)
+        {
+            if (!indexMap.test(i))
+            {
+                indexMap.set(i);
+                p = ((DataType *)(&storage) + i);
+                break;
+            }
+        }
+
+        size_t count = 1;
+        for (size_t j = i + 1; j < indexMap.size(); ++j)
+        {
+            if (count < size)
+            {
+                if (indexMap.test(j))
+                {
+                    p = nullptr;
+                    break;
+                }
+                else
+                {
+                    indexMap.set(j);
+                    ++count;
+                }
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        return p;
+    }
+
+    void deallocate()
+    {
+        // TODO: implement
+    }
+
+    void deallocate(DataType *p, size_t n)
+    {
+        size_t i = 0;
+
+        for (; i < indexMap.size(); i++)
+        {
+            if (p == ((DataType *)(&storage) + i))
+            {
+                indexMap.reset(i);
+                break;
+            }
+        }
+
+        size_t count = 1;
+        for (size_t j = i + 1; j < indexMap.size(); ++j)
+        {
+            if (count < n)
+            {
+                if (indexMap.test(j))
+                {
+                    indexMap.reset(j);
+                    ++count;
+                }
+                else
+                {
+                    std::logic_error("Error deallocating");
+                }
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
+
+private:
+    static constexpr unsigned int MAX_DATA_SIZE = 2048;
+    static constexpr size_t maxElem             = (MAX_DATA_SIZE / sizeof(Node));
+    unsigned char storage[MAX_DATA_SIZE];
+    static std::bitset<maxElem> indexMap;
+};
+
+template<typename DataType, typename Allocator = ListAllocator<DataType> >
+class CircularList
+{
+    // TODO: implement CircularList
+
+public:
+    typedef ListNode<DataType> Node;
+
+    // Adds new element to the end of the list
+    void push_back(DataType value)
+    {
+        push_front(value);
+        last = last->next;
+    }
+
+    // Adds new element to the beginning of the list
+    void push_front(DataType value)
+    {
+        typename Allocator::template rebind<Node> nodeAllocator;
+        Node *new_node = nodeAllocator.allocate(1);
+        if (new_node == nullptr)
+        {
+            return;
+        }
+
+        new_node->value = value;
+        if (last == nullptr)
+        {
+            last       = new_node;
+            last->next = last;
+        }
+        else
+        {
+            new_node->next = last->next;
+            last->next     = new_node;
+        }
+    }
+
+    // Inserts new element after provided node
+    void insert_after(Node *node, DataType value)
+    {
+        typename Allocator::template rebind<Node> nodeAllocator;
+        Node *new_node = nodeAllocator.allocate(1);
+        new_node->next = node->next;
+        node->next     = new_node;
+    }
+
+    // Removes last element from the list and returns it
+    DataType pop_back()
+    {
+        if (last == nullptr)
+        {
+            throw std::logic_error("Nothing to pop");
+        }
+
+        Node *new_last = last->next;
+        while (new_last->next != last)
+        {
+            new_last = new_last->next;
+        }
+
+        Node *head = last->next;
+        typename Allocator::template rebind<Node> nodeAllocator;
+        DataType value = last->value;
+        nodeAllocator.deallocate(last, 1);
+        new_last->next = head;
+        last           = new_last;
+        return value;
+    }
+
+    // Removes first element from the list end returns it
+    DataType pop_front()
+    {
+        if (last == nullptr)
+        {
+            throw std::logic_error("Nothing to pop");
+        }
+
+        Node *new_head = last->next->next;
+        typename Allocator::template rebind<Node> nodeAllocator;
+        DataType value = last->next->value;
+        nodeAllocator.deallocate(last->next, 1);
+        last->next = new_head;
+        return value;
+    }
+
+    size_t size() const
+    {
+        if (last == nullptr)
+        {
+            return 0;
+        }
+
+        Node  *p     = last->next;
+        size_t count = 1;
+        while (p != last)
+        {
+            p = p->next;
+            ++count;
+        }
+
+        return count;
+    }
+
+private:
+    static Allocator allocator;
+    Node *last = nullptr;
+};
+
+template<typename DataType>
+std::bitset<ListAllocator<DataType>::maxElem> ListAllocator<DataType>::indexMap;
---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include "CircularList.h"
+#include <random>
+#include <iostream>
+
+int main()
+{
+    constexpr size_t MAX_ELEMENTS = 5;
+    CircularList<unsigned long> firstIntList;
+    CircularList<int>  secondIntList;
+    CircularList<char> firstCharList;
+    std::random_device dev;
+    std::mt19937 rng(dev());
+    std::uniform_int_distribution<std::mt19937::result_type> dist(0, 100);
+
+    for (size_t i = 0; i < MAX_ELEMENTS; ++i)
+    {
+        unsigned long num = dist(rng);
+        std::cout << "Push back : " << num << std::endl;
+        firstIntList.push_back(num);
+
+        // num = dist(rng);
+        std::cout << "Push front : " << num << std::endl;
+        firstIntList.push_front(num);
+    }
+    for (size_t i = 0; i < MAX_ELEMENTS; ++i)
+    {
+        std::cout << "Pop back : " << firstIntList.pop_back() << std::endl;
+        std::cout << "Pop front : " << firstIntList.pop_front() << std::endl;
+    }
+
+    return 0;
+}
